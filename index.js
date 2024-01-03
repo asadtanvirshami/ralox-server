f7769303753da8eac5f3ea6bc4d68718b9ada642
Asad Tanvir Shami <91515695+asadtanvirshami@users.noreply.github.com>
2024-01-03 13:56:41 -0800

    orm and backend setup

---
+++ index.js
@@ -0,0 +1,40 @@
+// Importing required packages and modules
+const bodyParser = require('body-parser');
+const express = require("express");
+const morgan = require('morgan');
+const db = require("./models");
+const cors = require('cors');
+
+// Creating an instance of the Express application
+const app = express();
+
+// const { Appointments, AppointmentServices } = require("./associations/appointmentAssociations")
+
+// Importing route modules
+
+
+// Middleware setup
+app.use(morgan('tiny')); // Logging middleware for request details
+app.use(cors()); // Cross-Origin Resource Sharing for handling CORS issues
+
+// Body parsing middleware with extended options and increased limit
+app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+app.use(bodyParser.json({ limit: '100mb', extended: true }));
+app.use(express.json()); // JSON parsing middleware
+
+// Syncing database models
+db.sequelize.sync();
+
+// Setting up basic route for root endpoint
+app.get("/", (req, res) => {
+  res.json('Algorim Server');
+});
+
+// Setting up modular routes for different features
+
+
+// Configuring the server to listen on a specific port
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+});
--- models/index.js
+++ models/index.js
@@ -2,29 +2,29 @@
 
 const fs = require('fs');
 const path = require('path');
-const Sequelize = require('sequelize');
+const Sequelize = require('sequelize-cockroachdb');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+//const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
 let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+
+const connectionString = "postgresql://algorimsoftware:4Bi5g88B9Zx4exEL_J4ZOQ@sparse-mummy-7987.8nk.cockroachlabs.cloud:26257/defaultdb?sslmode=verify-full"
+sequelize = new Sequelize(connectionString, {
+  dialectOptions: {
+    application_name: "docs_simplecrud_node-sequelize",
+  },
+  logging:false
+});
+
+// 4Bi5g88B9Zx4exEL_J4ZOQ
 
 fs
   .readdirSync(__dirname)
   .filter(file => {
-    return (
-      file.indexOf('.') !== 0 &&
-      file !== basename &&
-      file.slice(-3) === '.js' &&
-      file.indexOf('.test.js') === -1
-    );
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
   })
   .forEach(file => {
     const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
